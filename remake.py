72d4b25ee53dfa1a358be40d8ff5e6dc92f0e28b
shiwirockztar <shiwirockztar@gmail.com>
2021-08-10 11:09:35 -0500

    nueva version del juego de carros ligera

---
+++ remake.py
@@ -0,0 +1,4 @@
+import sys          #libreria para limpiar pantalla
+import time         #libreria para el uso del time.sleep()
+import random       #libreria para el uso de los numero aleatorios
+import os           #libreria para el uso del os.system('cls')
